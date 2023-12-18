e112c6337fa42a709f9a4df8bf6f972fd633106d
Julian Gonzalo Fernandez <julyan_1@hotmail.com>
2023-12-18 09:20:22 -0300

    Add files via upload

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -32,6 +32,7 @@ func New(ctx context.Context, config config.Configuration) *Storage {
 	}
 
 	if err := client.Database("tweet").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		log.Fatalf("do a ping in db: %w", err)
 		panic(err)
 	}
 	color.Green("You successfully connected to MongoDB!")
